22b3299fd373e094dcbfb7b75efb56f7630e88cf
nyplrdgrz <Rodriguescount@gmail.com>
2025-11-12 18:12:35 -0300

    fix API_BASE

--- apiConfig.ts
+++ apiConfig.ts
@@ -1,4 +1,4 @@
 // export const API_BASE ='https://rarely-waterish-lenard.ngrok-free.dev';
-const API_BASE ='https://rarely-waterish-lenard.ngrok-free.dev';
+const API_BASE ='http://127.0.0.1:8000';
 
 export default API_BASE;
